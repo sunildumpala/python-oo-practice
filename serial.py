a2438e97c207611934a3df40b77f3c0a9771d173
sunildumpala <dumpala@gmail.com>
2024-01-30 13:41:31 -0800

    serial.py working

--- serial.py
+++ serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+
+    def __init__(self, start):
+        "Constructor for SerialGenerator, accepts the starting serial number"
+        self.serial = start - 1
+        self.start = start
+
+    def generate(self):
+        "Generate and return the next sequence serial number"
+        self.serial = self.serial+1
+        return self.serial
+    
+    def reset(self):
+        "Reset to the original starting number"
+        self.serial = self.start - 1
+
+
