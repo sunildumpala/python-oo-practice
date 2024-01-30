b6d5494bb65347ec032ab84f006a94e7281ac2b0
sunildumpala <dumpala@gmail.com>
2024-01-30 15:01:07 -0800

    wordfinder working

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,26 @@
+import random
+
 """Word Finder: finds random words from a dictionary."""
 
 
 class WordFinder:
-    ...
+    """
+    Class to read and maintain a list of words from a file
+    """
+
+    def __init__(self, filepath):
+        """ Constructor to initialize class by reading the contents of file at the filepath
+        """
+        file = open(filepath)
+        # self.text = file.read()
+        self.text = []
+        for line in file:
+            self.text.append(line.strip())
+        file.close
+
+    def getText(self):
+        return self.text
+    
+    def random(self):
+        """ Returns a random word(element) from the file read from the filepath(array)"""
+        return random.choice(self.text)
