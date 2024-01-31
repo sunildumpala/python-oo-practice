b114ccb7386f9e43d5b12cba533ba94e6eaf51cc
sunildumpala <dumpala@gmail.com>
2024-01-30 16:02:59 -0800

    SpecialWordFinder working

--- wordfinder.py
+++ wordfinder.py
@@ -12,11 +12,18 @@ class WordFinder:
         """ Constructor to initialize class by reading the contents of file at the filepath
         """
         file = open(filepath)
-        # self.text = file.read()
-        self.text = []
-        for line in file:
-            self.text.append(line.strip())
-        file.close
+        self.text = self.parse(file)
+        # self.text = []
+        # for line in file:
+        #     self.text.append(line.strip())
+        # file.close
+
+    def parse(self, dict_file):
+        text = []
+        for line in dict_file:
+            text.append(line.strip())
+        return text
+
 
     def getText(self):
         return self.text
@@ -24,3 +31,13 @@ class WordFinder:
     def random(self):
         """ Returns a random word(element) from the file read from the filepath(array)"""
         return random.choice(self.text)
+
+class SpecialWordFinder(WordFinder):
+    """ Subclass of WordFinder, can read and clean files with blanks and comments"""
+
+    def parse(self, dict_file):
+        text = []
+        for line in dict_file:
+            if (line.strip() and not line.startswith("#")):
+                text.append(line.strip())
+        return text
